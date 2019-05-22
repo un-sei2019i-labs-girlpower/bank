cae7a08a06c62d64453c9b3a4bfc83d452a6b5a0
MafeCarbo <mcarbonells@unal.edu.co>
2019-05-22 08:59:48 -0500

    v 0.0.3

--- mobile-app/bankk/main.cpp
+++ mobile-app/bankk/main.cpp
@@ -4,7 +4,30 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout<<"hola ya puedo entrar"<< endl;
+    int opcion, a , b;
+    bool flag;
+    while (flag==true){
+    cout << "Bienvenido a la calculadora, porfavor elija una opcion: " << endl;
+    cout << "1. Sumar 2 numeros." << endl;
+    cout << "2. Restar 2 numeros." << endl;
+    cout << "3. Multiplicar 2 numeros." << endl;
+    cout << "4. Dividir 2 numeros." << endl;
+    cin<< opcion;
+    switch(opcion){
+        case 1: {
+        }
+        case 2: {
+        }
+        case 3: {
+        }
+        case 4: {
+        }
+        default : {
+        }
+    }
+    cout << "Si desea realizar otra operacion oprima 1, si desea salir oprima 0" << endl;
+    cin<<flag;
+    }
+
     return 0;
 }
