13a54c54128c555939969209b1e056f2a75d52bd
DanielaDuque <30605041+DanielaDuque@users.noreply.github.com>
2019-05-30 10:53:44 -0500

    V1.1.11

--- mobile-app/Bank-app/app/src/main/java/com/example/bank_app/DataAccess/Models/Account.java
+++ mobile-app/Bank-app/app/src/main/java/com/example/bank_app/DataAccess/Models/Account.java
@@ -1,25 +1,31 @@
 package com.example.bank_app.DataAccess.Models;
 
 public class Account {
-    private int number;
-    private  int id_administrator;
+    private int account_number;
+    private  int id_admin;
+    private  int id_user;
     private double amount; //////////
 
-    public Account(int number, int id_administrator, double amount) {
-        this.number = number;
-        this.id_administrator = id_administrator;
+    public Account(int number, int id_administrator, double amount, int id_user) {
+        this.account_number = number;
+        this.id_admin = id_administrator;
         this.amount = amount;
+        this.id_user = id_user;
     }
 
-    public int getNumber() {
-        return number;
+    public int getAccount_number() {
+        return account_number;
     }
 
-    public int getId_administrator() {
-        return id_administrator;
+    public int getId_admin() {
+        return id_admin;
     }
 
     public double getAmount() {
         return amount;
+    }
+
+    public int getId_user() {
+        return id_user;
     }
 }
