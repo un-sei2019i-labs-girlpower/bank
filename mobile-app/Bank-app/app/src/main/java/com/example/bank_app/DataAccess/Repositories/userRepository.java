a1cd7376fa750d1673ca4f741ee679ab3713488d
MafeCarbo <mcarbonells@unal.edu.co>
2019-05-30 11:02:41 -0500

    V1.1.12

--- mobile-app/Bank-app/app/src/main/java/com/example/bank_app/DataAccess/Repositories/userRepository.java
+++ mobile-app/Bank-app/app/src/main/java/com/example/bank_app/DataAccess/Repositories/userRepository.java
@@ -1,7 +1,27 @@
 package com.example.bank_app.DataAccess.Repositories;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.bank_app.DataAccess.DataBase.Database;
+import com.example.bank_app.DataAccess.Models.Account;
+import com.example.bank_app.DataAccess.Models.User;
+
 public class userRepository {
-    private boolean create_user (){return true;};
+    Context context;
+    Database db;
+    public userRepository(Context context) {
+        this.context= context;
+        this.db = new Database(context);
+    }
+    private boolean createUser (User user){
+        SQLiteDatabase database = db.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        //values.put("IDENTIFICATION_USER",);
+        database.insert("USER",null,values);
+        return true;
+    };
     private int getUserById(){return 0;};
     private boolean updateUser(){return true;};
     private boolean delateUser (){return true;};
