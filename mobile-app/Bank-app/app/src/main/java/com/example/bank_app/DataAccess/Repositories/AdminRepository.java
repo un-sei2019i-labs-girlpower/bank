a69c6d644be3d7dc29f483467081b07af7a43aec
DanielaDuque <30605041+DanielaDuque@users.noreply.github.com>
2019-05-30 11:03:11 -0500

    V.1.13

--- mobile-app/Bank-app/app/src/main/java/com/example/bank_app/DataAccess/Repositories/AdminRepository.java
+++ mobile-app/Bank-app/app/src/main/java/com/example/bank_app/DataAccess/Repositories/AdminRepository.java
@@ -1,4 +1,27 @@
 package com.example.bank_app.DataAccess.Repositories;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.bank_app.DataAccess.DataBase.Database;
+import com.example.bank_app.DataAccess.Models.Account;
+import com.example.bank_app.DataAccess.Models.Admin;
+
 public class AdminRepository {
+    Context context;
+    Database db;
+
+    public AdminRepository(Context context) {
+        this.context= context;
+        this.db = new Database(context);
+    }
+
+    private boolean create_accout (Admin admin){
+        SQLiteDatabase database = db.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        //values.put("ID",);
+        database.insert("ACCOUNT",null,values);
+        return true;
+    };
 }
